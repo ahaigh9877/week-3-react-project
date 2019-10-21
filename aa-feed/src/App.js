ea24ffbac667eb99e17d3e5617773a3fd197a7af
ahaigh9877 <alastairhaigh@me.com>
2019-10-21 17:23:19 +0200

    included the other stuff

---
+++ aa-feed/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./App.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Content from "./components/content";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header></Header>
+      <Content></Content>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
