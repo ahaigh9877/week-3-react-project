50ac06fb2879eba984e5ed835ff34a14969847cd
ahaigh9877 <alastairhaigh@me.com>
2019-10-22 17:18:10 +0200

    logo put in images dir

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { logo } from "../images/brewdog-logo.png";
 
 export default class Header extends React.Component {
   render() {
     return (
       <header>
-        <img className="logoImg" alt="logo"></img>
+        <img className="logoImg" alt="logo" src={logo}></img>
         <a href="#">About Us</a>
         <a href="#">Support</a>
         <a href="#">ITEM3</a>
