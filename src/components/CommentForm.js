62fbd6f455cbf1c98fd4232b02cdf8ddd49a85c5
apavlushina <ssemina7@gmail.com>
2019-10-25 11:22:54 +0200

    we have comments

--- src/components/CommentForm.js
+++ src/components/CommentForm.js
@@ -1,22 +1,59 @@
 import React, { Component } from "react";
+import Comment from "./Comment";
 
 class CommentForm extends Component {
+  state = {
+    comments: [{ id: 1, text: "I like it!" }],
+    onecomment: ""
+  };
+
+  addComment = text => {
+    const id = Math.round(Math.random() * 100000000);
+    const comments = [...this.state.comments, { id: id, text: text }];
+    this.setState({ comments: comments });
+  };
+
+  handleChange = event => {
+    this.setState({ onecomment: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.addComment(this.state.onecomment);
+    console.log("SUBMITTING", this.state);
+    this.setState({ onecomment: "" });
+  };
+
   render() {
     return (
-      <form>
-        <textarea
-          type="text"
-          name="commentArea"
-          placeholder="what do you think of this beer?"
-          onChange={this.handleChange}
-          value={this.comment}
-        />
-        <input
-          type="submit"
-          value="Submit your comment"
-          onClick={this.handleSubmit}
-        />
-      </form>
+      <div>
+        <div>
+          <h4>Comments</h4>
+          {this.state.comments.map(comment => {
+            return (
+              <Comment
+                key={comment.id}
+                id={comment.id}
+                content={comment.text}
+              />
+            );
+          })}
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <textarea
+            type="text"
+            name="commentArea"
+            placeholder="what do you think of this beer?"
+            onChange={this.handleChange}
+            value={this.state.onecomment}
+          />
+          <input
+            type="submit"
+            value="Submit your comment"
+            onClick={this.handleSubmit}
+          />
+        </form>
+      </div>
     );
   }
 }
